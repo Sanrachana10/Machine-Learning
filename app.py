fa218b1374b64be47acd14039016be76103ce42b
Sanrachana10 <singhsanrachana1011@gmail.com>
2026-04-09 22:15:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -412,7 +412,7 @@ body { background: #080c14; }
     <div class="college-heading">Shri G.S. Institute of Technology &amp; Science</div>
     <div class="dept-subtitle">Department of <strong>Information Technology</strong></div>
     <div class="event-tag">&#9632; Lab Exhibition 2026 &nbsp;·&nbsp; Live</div>
-    <img src="https://tse4.mm.bing.net/th/id/OIP.5mJW-asixFrgwq9fIXkOVwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:70px; margin-top:16px; opacity:0.9;" />
+    <img src="https://tse4.mm.bing.net/th/id/OIP.5mJW-asixFrgwq9fIXkOVwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" style="display:block;width:80px; margin-top:16px; opacity:0.9;" />
     <div class="header-rule"></div>
   </div>
 
